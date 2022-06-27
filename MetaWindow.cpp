5dce809178ac94212d1154be5bc9b938cb0157e0
BtwX28 <100072844+BtwX28@users.noreply.github.com>
2022-06-27 07:33:40 +0200

    Update MetaWindow.cpp

--- MetaWindow.cpp
+++ MetaWindow.cpp
@@ -1,7 +1,7 @@
 #include <SDL.h>
 
 int main() {
-    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Init(SDL_INIT_EVERYTHING);
     SDL_Window *win = SDL_CreateWindow("Hello World!", 100, 100, 300, 300, SDL_WINDOW_SHOWN);
     SDL_Delay(5000);
     return 0;
