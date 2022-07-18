325937b56413f171f9afe6517df6f74602daf426
Concept Pro <concept1928@gmail.com>
2022-07-18 08:29:34 +0200

    Changed Window Flag

--- src/App.cpp
+++ src/App.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[]) {
     SDL_Init(SDL_INIT_EVERYTHING);
     //asdasd
 
-    SDL_Window *window = SDL_CreateWindow("MetaBallers", 100, 100, WIDTH, HEIGHT, SDL_WINDOW_RESIZABLE);
+    SDL_Window *window = SDL_CreateWindow("MetaBallers", 100, 100, WIDTH, HEIGHT, 0);
 
     if (NULL == window) {
         std::cout << "Could not create window: " << SDL_GetError() << std::endl;
